48eb4f1602f1ba7e22e1f06eb04604c6dc3671fe
Alex Ciobica <alex.ciobica@gmail.com>
2018-11-17 10:12:26 +0200

    Add boilerplate for filtering.

--- src/App.js
+++ src/App.js
@@ -3,33 +3,62 @@ import styled, { css } from "styled-components";
 import { Redirect, withRouter } from "react-router";
 import { Route, Switch } from "react-router-dom";
 import Async from "react-async";
+import { fetchCards } from "./helpers";
 
-import { fetchCards, DataContext } from "./helpers";
+import { DataContext } from "./helpers";
 import Header from "./components/Header";
 import RouteMain from "./RouteMain";
 import RouteContact from "./RouteContact";
 
-// The app is too simple to justify breaking the rendering components
-// to separate files
 class App extends Component {
+  state = {
+    data: [],
+    filter: false
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.data !== state.data) {
+      return {
+        data: props.data
+      };
+    }
+    return null;
+  }
+
+  setFilter(letter) {
+    this.setState({
+      filter: letter.toUpperCase()
+    });
+  }
+
+  getData() {
+    if (this.state.filter) {
+      return this.state.data.filter(
+        contact => contact.name.charAt(0).toUpperCase() === this.state.filter
+      );
+    }
+    return this.state.data;
+  }
+
   render() {
     return (
-      <Async promiseFn={fetchCards}>
-        {({ data, error, isLoading }) => {
-          if (isLoading) return "Loading...";
-          if (error) return error.message;
-          if (data.length)
-            return (
-              <DataContext.Provider value={data}>
-                <Header>My Address Book</Header>
-                <Route path="/" exact component={RouteMain} />
-                <Route path="/contact/:contact" component={RouteContact} />
-              </DataContext.Provider>
-            );
-        }}
-      </Async>
+      <DataContext.Provider value={this.getData()}>
+        <Header>My Address Book</Header>
+        <Route path="/" exact component={RouteMain} />
+        <Route path="/contact/:contact" component={RouteContact} />
+      </DataContext.Provider>
     );
   }
 }
 
-export default App;
+export default function Provider() {
+  return (
+    <Async promiseFn={fetchCards}>
+      {({ data, error, isLoading }) => {
+        if (isLoading) return "Loading...";
+        if (error) return error.message;
+        if (data) return <App data={data} />;
+      }}
+    </Async>
+  );
+}
